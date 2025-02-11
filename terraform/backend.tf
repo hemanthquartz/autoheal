fd7979c582a4d675f0b535886a151d0bdc94c7bc
hemanthquartz <71367565+hemanthquartz@users.noreply.github.com>
2025-02-10 23:38:42 -0500

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "azurerm" {
     resource_group_name   = "openai_rg"
     storage_account_name  = "tfbackendmanual"
-    container_name        = "tfstate"
+    container_name        = "autohealtfstate"
     key                   = "terraform.tfstate"
   }
 }
